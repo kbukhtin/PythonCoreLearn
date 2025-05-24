206001f669d58dbcfca243c644616af854157047
kbukhtin <buxtin.kir@yandex.ru>
2025-05-24 21:58:32 +0300

    Модуль 1 | Урок 5: - выполнены задания с 6-11

---
+++ module_01/lesson_05(Function input())/ex_01.05.11.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+value_1 = a + b + c
+value_2 = a * b + c
+value_3 = a + b * c
+value_4 = a * b * c
+value_5 = (a + b) * c
+value_6 = a * (b + c)
+
+print(max(value_1, value_2, value_3, value_4, value_5, value_6))
