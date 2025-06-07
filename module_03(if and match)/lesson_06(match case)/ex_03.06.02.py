c236e64cce2a31382e72c3fdce2321ead2ae1f20
kbukhtin <buxtin.kir@yandex.ru>
2025-06-07 19:03:22 +0300

    Модуль 3 | Урок 6: - выполнены все задания

---
+++ module_03(if and match)/lesson_06(match case)/ex_03.06.02.py
@@ -0,0 +1,10 @@
+n = int(input())
+match n:
+    case 1 | 3 | 5 | 7 | 8 | 10 | 12:
+        print(31)
+    case 4 | 6 | 9 | 11:
+        print(30)
+    case 2:
+        print(28)
+    case _:
+        print('Давай еще раз!')
